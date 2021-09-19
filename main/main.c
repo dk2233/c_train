b9f523dd661c5e7af32d3271ef03df27758a4d92
kucharsd <daniel.kucharski@zf.com>
2021-09-19 19:26:05 +0200

    updated function to open file and return its pointer via argument

--- main/main.c
+++ main/main.c
@@ -4,6 +4,7 @@
 #include "str_change.h"
 #include "func_pointers.h"
 #include "different_functions.h"
+#include "defines.h"
 
 
 int main(int argc ,char *argv[])
@@ -44,8 +45,6 @@ int main(int argc ,char *argv[])
 
     call_one_function_pointer(&sum_to_int, 3 , 4);
 
-
-
     int a =2;
     int b = 5;
     char *str1 = "tekst - testowy !? nie!@";
@@ -58,8 +57,9 @@ int main(int argc ,char *argv[])
     char *str_from_file = calloc(1000U, sizeof(char));
     str_line((void *)file_hd,(void *) str_from_file);
     printf("1. first line %s \n", str_from_file) ;
+    fclose(file_hd);
 
-    FILE * file_hd2;
+    FILE *file_hd2;
     function_pointers_definition functions_def_struct = 
     {
         .elements_number = HOW_MANY_FUNCTION,
@@ -76,18 +76,28 @@ int main(int argc ,char *argv[])
             (void*)&sss,
 
         },
+        /*  this method of using pointer to return File will not work
+         *  every time we open file we also change structure address
+         *  so we should use here ** pointer that can be changed were it is pointing to */
         .func_array[2] ={ &func_open_file,  (void *)file_name, (void *)file_hd2}, 
     };
 
     call_all_function( &functions_def_struct);
 
-    printf("file handler %ld \n", *(long *)file_hd2);
+    printf("file handler bef  wrong one %ld \n", (long*)file_hd2);
+/*      THIS_DOES_NOT_WORK read_one_line_from_file(file_name, file_hd2);*/
+    /*  file_hd2 is pointing to wrong address */
+    func_open_file_FILE((void*)file_name, &file_hd2);
+    
+    read_one_line_from_file(file_name, file_hd2);
+
+
+    printf("file handler 2 %ld \n", (long*)file_hd2);
 
     str_from_file = calloc(1000U, sizeof(char));
     str_line((void *)file_hd2,(void *) str_from_file);
     printf("2. first line %s \n", str_from_file) ;
 
-    fclose(file_hd);
     fclose(file_hd2);
 
     free(str_from_file);
