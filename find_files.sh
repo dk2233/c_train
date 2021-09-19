e3a3b9c0a876ab8038ac1353e93cf52769c281fd
dk2233 <kucharski22daniel@gmail.com>
2021-09-19 16:13:25 +0200

    corrected find files shell script on tablet

--- find_files.sh
+++ find_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
 find . -name '*.c' > cscope.files
 find . -name '*.h' >> cscope.files
-
+find ../../../usr/include/std*  -name '*.h' >> cscope.files
+find ../../../usr/include/str*  -name '*.h' >> cscope.files
+find ../../../usr/include/int*  -name '*.h' >> cscope.files
