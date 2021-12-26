0b7fa6e154c71893cd988c37e99eff096c7428d9
danielku <kucharski22daniel@gmail.com>
2021-12-26 01:05:15 +0100

    added find linux includes

---
+++ find_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+find . -name '*.c' > cscope.files
+find . -name '*.h' >> cscope.files
+find /usr/include/std*  -name '*.h' >> cscope.files
+find /usr/include/str*  -name '*.h' >> cscope.files
+find /usr/include/int*  -name '*.h' >> cscope.files
