c4742addefcb608e6b0eb97432cf33d281fcc211
dk2233 <kucharski22daniel@gmail.com>
2023-04-09 20:19:44 +0200

    updated arg conifg with new version of c_arg_parse

--- argv_parse/arg_parse_config.c
+++ argv_parse/arg_parse_config.c
@@ -2,11 +2,11 @@
 #include "arguments_functions.h"
 
 argument_config_t arg_config[] = {
-    {"-a" , .function_to_parse.function_arg_array = argument_array, "give an array size to be listed"},
-    {"-rb" , .function_to_parse.function_arg_array = argument_file_read_point, "read file -rb<file name>"},
-    {"-hash=",.function_to_parse.function_arg_array = argument_hashes, "calculate hash using dbj"},
-    {"-file=",.function_to_parse.function_arg_array = argument_file_read, "open text file and read some line"},
-    {"-fs=",.function_to_parse.function_arg_array = argument_file_size,"check file size with time measurement"},
-    {"", NULL, NULL}, /* this line is a marker of last element*/
+    {"-a" , .function_to_parse.function_arg_char = argument_array, "give an array size to be listed", 0, 0},
+    {"-rb" , .function_to_parse.function_arg_char = argument_file_read_point, "read file -rb<file name>", 0, 0},
+    {"-hash=",.function_to_parse.function_arg_char = argument_hashes, "calculate hash using dbj", 0, 0},
+    {"-file=",.function_to_parse.function_arg_char = argument_file_read, "open text file and read some line",0 ,0},
+    {"-fs=",.function_to_parse.function_arg_char = argument_file_size,"check file size with time measurement",0 ,0},
+    {"", NULL, NULL, 0, 0}, /* this line is a marker of last element*/
 
 };
