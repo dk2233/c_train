fc1b4bef8a292e8154ba11b696d54ccfc379354c
dakuch <kucharski22daniel@gmail.com>
2023-08-26 09:44:29 +0200

    challenge 1 & 2 from udemy course

--- pthread/pthread_test.c
+++ pthread/pthread_test.c
@@ -6,10 +6,28 @@
 #include "stdint.h"
 
 void * function_4_thread(void * input);
+void * threads_loop(void * input);
+void * fun_thread1(void *arg);
+void *fun_ch1_thread(void *arg);
+void pthread_challenge1(void);
+void *fun_ch2_thread(void *arg);
+void pthread_challenge2(void);
 
 #define MAX_THREADS   5u
+#define CH1_THREADS_NUMBER 10u
 
 static int result = 0;
+static pthread_mutex_t lock_mutex;
+static pthread_cond_t condition_var;
+static int counter_ch1 = 0;
+
+
+void * fun_thread1(void *arg)
+{
+    /* check the order */
+    printf("2.Jason \n");
+    pthread_exit("Bye");
+}
 
 void * function_4_thread(void * input)
 {
@@ -35,8 +53,12 @@ void * function_4_thread(void * input)
 
 void * threads_loop(void * input)
 {
-
     uint32_t j = 0u;
+    /* disable this line to see a mixed work of both pthread
+    
+    in case this line exists - first thread locks and make second thread to be halted*/
+    pthread_mutex_lock(&lock_mutex);
+    
     for(uint32_t i = 0u; i< *(uint32_t *) input ; i++ )
     {
         j++;
@@ -47,6 +69,7 @@ void * threads_loop(void * input)
 
 
     }
+    pthread_mutex_unlock(&lock_mutex);
 
 }
 
@@ -56,7 +79,7 @@ void pthread_test(void)
      int return_value = 0;
      int *return_value_p = &return_value;
      int data = 24;
-     uint32_t how_many_repeats[2u] = {2000u, 20000u} ;
+     uint32_t how_many_repeats[2u] = {5000u, 20000u} ;
     size_t stack_size;
 
 
@@ -76,6 +99,8 @@ void pthread_test(void)
 
 
     */
+   pthread_mutex_init(&lock_mutex, NULL);
+
     if (pthread_create(&(pthread1[0]), &attr_pthread_local , &function_4_thread, NULL) != 0)
     {
         printf("problem with thread creating\n");
@@ -109,8 +134,99 @@ void pthread_test(void)
     pthread_join(pthread1[0], NULL);
     pthread_join(pthread1[1], NULL);
 
-    
+
+    if (pthread_create(&(pthread1[2]), NULL, fun_thread1, NULL) != 0)
+    {
+        perror("pthread_create error \n");
+    }
+    printf("1.Fedin \n");
+
+    if (pthread_join(pthread1[2], NULL) != 0)
+    {
+        perror("pthread_join error");
+    }    
+
+    pthread_challenge1();
+    pthread_challenge2();
 
 }
 
+void *fun_ch1_thread(void *arg)
+{
+    printf("message %d, thread %ld, read counter %d\n", *(uint16_t *)arg, pthread_self(), counter_ch1 );
+    counter_ch1++;
+    printf("read counter after increased %d \n", counter_ch1);
+}
 
+void *fun_ch2_thread(void *arg)
+{
+    pthread_mutex_lock(&lock_mutex);
+    printf("message %d, thread %ld, read counter %d\n", *(uint16_t *)arg, pthread_self(), counter_ch1 );
+    counter_ch1++;
+    printf("read counter after increased %d \n", counter_ch1);
+    pthread_mutex_unlock(&lock_mutex);
+}
+/*
+
+this function shows that in such case like defined here
+global counter change will not have any specific order every time
+you run function again - you should never expect any specific behaviour with threads
+
+unless you use them in specific way
+
+*/
+void pthread_challenge1(void)
+{
+    pthread_t pthread_ch1_a[CH1_THREADS_NUMBER] = {0u};
+    static uint16_t message_array[CH1_THREADS_NUMBER] = {0u};
+    static uint16_t i = 0u;
+    counter_ch1 = 0u;
+
+    perror("\nchallenge1\n");
+    for(i = 0u; i < CH1_THREADS_NUMBER; i++)
+    {
+        message_array[i] = i;
+        if (pthread_create(&(pthread_ch1_a[i]), NULL, fun_ch1_thread, (void*)&(message_array[i])  ) != 0)
+        {
+            printf("problem creating thread %d\n",i);
+        }
+    }
+
+
+    for( i = 0u; i < CH1_THREADS_NUMBER ; i++)
+    {
+        if (pthread_join(pthread_ch1_a[i], NULL) != 0)
+        {
+            printf("problem joining %d",i);
+        }
+    }
+
+
+}
+
+void pthread_challenge2(void)
+{
+    pthread_t pthread_ch2_a[CH1_THREADS_NUMBER] = {0u};
+    static uint16_t message_array[CH1_THREADS_NUMBER] = {0u};
+    static uint16_t i = 0u;
+    counter_ch1 = 0u;
+
+    perror("\nchallenge 2\n");
+    for(i = 0u; i < CH1_THREADS_NUMBER; i++)
+    {
+        message_array[i] = i;
+        if (pthread_create(&(pthread_ch2_a[i]), NULL, fun_ch2_thread, (void*)&(message_array[i])  ) != 0)
+        {
+            printf("problem creating thread %d\n",i);
+        }
+    }
+
+
+    for( i = 0u; i < CH1_THREADS_NUMBER ; i++)
+    {
+        if (pthread_join(pthread_ch2_a[i], NULL) != 0)
+        {
+            printf("problem joining %d",i);
+        }
+    }
+}
